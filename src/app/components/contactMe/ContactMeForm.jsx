bc0f587a0fd7b4123c5ff8f8c5ea530291f50c4a
Temitayo <linuxmathew1245@gmail.com>
2025-01-26 15:21:02 +0100

    add: footer section to the homepage

--- src/app/components/contactMe/ContactMeForm.jsx
+++ src/app/components/contactMe/ContactMeForm.jsx
@@ -3,24 +3,63 @@ import "./ContactMeForm.css";
 
 const ContactMeForm = () => {
   return (
-    <div className="text-center form-wrapper">
-      <p className="formHead">Send me a message</p>
-      <p className="formDesc">
-        Got a question or proposal, or just want to say hello? Go ahead.
-      </p>
-      <div className="d-flex">
-        <div className="form-group">
-          <label style={{ color: "#86869B" }} for="exampleFormControlInput1">
-            Email address
-          </label>
-          <input
-            style={{ backgroundColor: "transparent" }}
-            border="0px 0px 1px 0px"
-            type="email"
-            className="form-control"
-            id="exampleFormControlInput1"
-            placeholder="name@example.com"
-          />
+    <div className="container-md" style={{}}>
+      <div className="mx-auto d-flex justify-content-center ">
+        <div className="form-wrapper px-auto">
+          <p className="formHead text-center">Send me a message</p>
+          <p className="formDesc text-center">
+            Got a question or proposal, or just want to say hello? Go ahead.
+          </p>
+          <div className="flex-wrapper d-flex justify-content-between">
+            <div className="form-group ">
+              <label
+                style={{ color: "#86869B" }}
+                htmlFor="exampleFormControlInput1"
+              >
+                Your name
+              </label>
+              <input
+                type="email"
+                className="form-input form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+              />
+            </div>
+            <div className="form-group ">
+              <label
+                style={{ color: "#86869B" }}
+                htmlFor="exampleFormControlInput1"
+              >
+                Email
+              </label>
+              <input
+                style={{
+                  backgroundColor: "transparent",
+                  border: "none",
+                  borderBottom: "1px solid var(--text-color-neutral-border)",
+                  borderRadius: "0px",
+                }}
+                type="email"
+                className="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+              />
+            </div>
+          </div>
+          <div className="text-area-wrapper">
+            <label
+              htmlFor="exampleFormControlTextarea1"
+              className="form-label"
+              style={{ color: "#86869B" }}
+            >
+              Example textarea
+            </label>
+            <textarea
+              className="text-area-form form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
         </div>
       </div>
     </div>
